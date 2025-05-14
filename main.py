d15aef1bf8aff32e2b658240fab95bf53cdd9b36
TheGreatHandini <pranavhanda1@gmail.com>
2025-05-13 19:30:48 -0500

    Letters sorted by count function added

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from stats import get_word_number
-from stats import count_characters
+from stats import get_word_number,count_characters,sort_dictionaries, sort_on
 
 def get_book_text (path):
     # gets the text from the book and returns in a string
@@ -11,7 +10,14 @@ def get_book_text (path):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_word_number(book_text)
-    print(f'{word_count} words found in the document')
-    print(count_characters(book_text))
+    sorted_dict = sort_dictionaries(count_characters(book_text))
+    print('''============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {word_count} total words
+--------- Character Count -------''')
+    for i in sorted_dict:
+        print(i)
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,14 @@ def count_characters(book_text):
         letter_count = book_text.count(letter)
         letter_dictionary[letter] = letter_count
     return letter_dictionary
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dictionaries(character_dict):
+    dictionary_list =[]
+    for key,value in character_dict.items():
+        new_dict = {"char": key, "num": value}
+        dictionary_list.append(new_dict)
+    dictionary_list.sort(reverse=True, key=sort_on )
+    return dictionary_list
